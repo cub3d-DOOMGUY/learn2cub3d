f440052e161d143c62edd1d68e3c23b0c213362c
scarf <greenscarf005@gmail.com>
2022-04-17 10:29:41 +0900

    chore: :truck: yay, move things

---
+++ makefile
@@ -0,0 +1,15 @@
+1:
+	make -C src/01_untextured_raycast/ run
+
+2:
+	make -C src/02_textured_raycast/ run
+
+3:
+	make -C src/03_img_textured_raycast/ run
+
+4:
+	make -C src/04_floor_ceiling/ run
+
+5:
+	make -C src/05_sprite_raycast/ run
+
---
+++ src/config.mk
@@ -0,0 +1,24 @@
+NAME := program.out
+LIB_PATH := ../../lib
+INC := -I$(LIB_PATH)
+LIB := -L$(LIB_PATH)/mlx_linux -lmlx -lXext -lX11 -lm
+CC := clang
+
+SRC = $(wildcard *.c)
+OBJ = $(SRC:.c=.o)
+
+%.o: %.c
+	$(CC) $(INC)  -c $< -o $@
+
+$(NAME): $(OBJ)
+	$(CC) $(INC) $^ $(LIB) -o $@
+
+all: $(NAME)
+
+run:
+	./$(NAME)
+
+clean:
+	rm -f $(NAME) *.o
+
+re: clean all
